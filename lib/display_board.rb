a20f3c4f85b05528395e7d25de4b146733f0b322
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-10 14:36:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,11 +11,11 @@ end
 
 def display_board(board)
   board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
- puts " #{board[0]} | #{board[1]} | #{board[2]}   "
+ puts " #{board[0]}  | #{board[1]} | #{board[2]}   "
  puts "-----------"
- puts " #{board[3]} | #{board[4]} | #{board[5]}   "
+ puts " #{board[3]}  | #{board[4]} | #{board[5]}   "
  puts "-----------"
- puts " #{board[6]} | #{board[7]} | #{board[8]}   " 
+ puts " #{board[6]}  | #{board[7]} | #{board[8]}   " 
 end
 
 
