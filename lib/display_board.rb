601e237b7607c4484d12cb1ac3145901382b68b5
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-10 14:56:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board)
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
-end
 
 def display_board(board)
  puts " #{board[0]}  | #{board[1]} | #{board[2]}   "
@@ -14,6 +7,7 @@ def display_board(board)
  puts " #{board[3]}  | #{board[4]} | #{board[5]}   "
  puts "-----------"
  puts " #{board[6]}  | #{board[7]} | #{board[8]}   " 
+ board[4] = "X"
 end
 
 
