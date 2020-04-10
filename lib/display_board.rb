360d4187e293e7924ac896dfb6b109fcf6a05bf0
Simba Mumanyi <simbamumanyi@gmail.com>
2020-04-10 14:30:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -16,7 +16,9 @@ def display_board(board)
  puts " #{board[3]}  | #{board[4]} | #{board[5]}   "
  puts "-----------"
  puts " #{board[6]}  | #{board[7]} | #{board[8]}   " 
+
 end
+
 
 
 def display_board(board)
